6223e8c2158a7cdc715553da5847ce34e17f29e6
Simona Šusterová <simsusterova@gmail.com>
2025-04-30 20:50:36 +0200

    Ctvrty ukol s 1 bonusem

--- hra.js
+++ hra.js
@@ -1,5 +1,6 @@
-let currentPlayer = 'circle'; 
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';  //přidání funkce findWinner
 
+let currentPlayer = 'circle'; 
 
 const aktualizujHrace = () => {
   const ikona = document.getElementById('current-player-icon');
@@ -14,38 +15,59 @@ const aktualizujHrace = () => {
 };
 
 
+
+//vyberu všechna pole hrací tabulky, každé pole bude zmapované co obsahuje
+const kontrolaViteze = () => {
+  const herniPole = Array.from(document.querySelectorAll('.cell')).map(cell => {
+    if (cell.classList.contains('board__field--circle')) {
+      return 'o';  
+    } else if (cell.classList.contains('board__field--cross')) {
+      return 'x';  
+    } else {
+      return '_';  
+    }
+  });
+
+ 
+  // rozhodnutí o výhře
+  const vitez = findWinner(herniPole);
+  
+  if (vitez === 'o' || vitez === 'x') {
+    alert(`Vyhrál hráč se symbolem '${vitez}'.`);
+    location.reload();
+
+  //BONUS - remíza
+  } else if (vitez === 'tie') {
+    alert('Je to remíza.');
+    location.reload();
+  }
+};
+  
 const klik = (event) => {
   const blok = event.target;
 
-  
+  if (blok.classList.contains('board__field--circle') || blok.classList.contains('board__field--cross')) {
+    return;  
+  }
+
   if (currentPlayer === 'circle') {
     blok.classList.add('board__field--circle');
     currentPlayer = 'cross'; 
   } else {
     blok.classList.add('board__field--cross');
     currentPlayer = 'circle'; 
-  }
+  };
 
   blok.disabled = true; 
   aktualizujHrace(); 
-};
 
+ //kontrola po každém 1 tahu
+  kontrolaViteze();
+};
 
 const policka = document.querySelectorAll('.cell');
 policka.forEach((policko) => {
   policko.addEventListener('click', klik);
 });
 
-
 aktualizujHrace();
-
-//BONUS - dotaz na restart
-const restartDotaz = document.querySelector('.restart');
-
-restartDotaz.addEventListener('click', (event) => {
-  const chceRestartovat = confirm('Opravdu chceš začít znovu?');
-
-  if (!chceRestartovat) {
-    event.preventDefault(); 
-  }
-});
