b043aa3b27350eb235ba1b3f738a5e389b7429d3
Simona Šusterová <simsusterova@gmail.com>
2025-05-11 19:18:34 +0200

    zkouška

--- hra.js
+++ hra.js
@@ -1,13 +1,10 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-
 // v tomto posledním úkolu sobě i pro přehlednost code review doplním sem tam komentáře, jelikož jsem něco přepsala,poupravila a sama se v tom už mírně ztrácela 
 
-// kdo je teď na tahu - začíná kolečko, tedy já
-let currentPlayer = 'circle';
- 
-//pak se střídá kolečko x krížek stále dokola, dokud nenastane remíza nebo výhra
+let currentPlayer = 'circle'; // kdo je teď na tahu - začíná kolečko, tedy já
 
+//pak se střídá kolečko x krížek stále dokola, dokud nenastane remíza nebo výhra
 const aktualizujHrace = () => {
   const ikona = document.getElementById('current-player-icon');
   if (currentPlayer === 'circle') {
@@ -33,14 +30,17 @@ const AITah = async (herniPole) => {
   });
 
   const data = await odpoved.json();
-  return data.position;
+  const index = data.position.x + data.position.y * 10;
+  //Na DOM elementu příslušného políčka zavolej metodu .click()
+  const policko = document.querySelectorAll('.cell')[index];
+
+  // kliknutí AI po určitém čas.úseku
+  setTimeout(() => {
+    policko.click();
+  }, 500);
 };
 
-  // přidáme malou prodlevu pro realističtější dojem
-
-
-//kontrola, zda nemáme už vítěze a nebo je tah na AI straně (křížek)
-
+//kontrola, zda nemáme už vítěze, a nebo je tah na AI straně (křížek)
 const kontrolaViteze = async () => {
   const herniPole = Array.from(document.querySelectorAll('.cell')).map(cell => {
     if (cell.classList.contains('board__field--circle')) {
@@ -52,10 +52,8 @@ const kontrolaViteze = async () => {
     }
   });
 
-
-// zde je ze 4. úkolu bonus - remíza
+  // zde je ze 4. úkolu bonus - remíza
 // pokud někdo vyhraje, zobrazí se hláška a aktualizuje se hra
-
   const vitez = findWinner(herniPole);
 
   if (vitez === 'o' || vitez === 'x') {
@@ -65,21 +63,11 @@ const kontrolaViteze = async () => {
     alert('Je to remíza.');
     location.reload();
   } else if (currentPlayer === 'cross') {
-    const pozice = await AITah(herniPole);
-    const index = pozice.x + pozice.y * 10;
-    const vybranePolicko = document.querySelectorAll('.cell')[index];
-
-    vybranePolicko.classList.add('board__field--cross');
-    vybranePolicko.disabled = true;
-    currentPlayer = 'circle';
-    aktualizujHrace();
-    await kontrolaViteze(); 
+    await AITah(herniPole); 
   }
 };
 
-
 // událost - kliknutí z mojí strany - kolečko
-
 const klik = async (event) => {
   const blok = event.target;
 
@@ -96,15 +84,19 @@ const klik = async (event) => {
     currentPlayer = 'cross';
     aktualizujHrace();
     await kontrolaViteze();
+  } else if (currentPlayer === 'cross') {
+    blok.classList.add('board__field--cross');
+    blok.disabled = true;
+    currentPlayer = 'circle';
+    aktualizujHrace();
+    await kontrolaViteze();
   }
 };
 
-
 // posluchač události na celou hrací plochu - hlídá kliknutí na políčka
-
 const policka = document.querySelectorAll('.cell');
 policka.forEach((policko) => {
   policko.addEventListener('click', klik);
 });
 
-aktualizujHrace();
+aktualizujHrace(); 
